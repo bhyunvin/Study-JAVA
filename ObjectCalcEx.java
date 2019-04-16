747bf9ccbd2191c3f82b79ffbc863d313776a006
백종현 <bhyunvin@naver.com>
2019-04-16 19:12:01 +0900

    Add files via upload

--- ObjectCalcEx.java
+++ ObjectCalcEx.java
@@ -6,6 +6,10 @@ class Calculator {
 	int left;
 	int right;
 
+	public void Calcurator() {
+	} // 없다면 자바에서 자동으로 생성될 기본생성자
+		// 메서드와 이름이 같음
+
 	public void setOp(int left, int right) {
 		this.left = left;
 		this.right = right;
@@ -18,21 +22,31 @@ class Calculator {
 	public void avg() {
 		System.out.println((this.left + this.right) / 2 + base);
 	}
+
 }
 
-public class ObjectCalcEx {
-	public static void main(String[] args) {
-		Calculator c1 = new Calculator();
-		c1.setOp(10, 20);
-		c1.sum();
-		c1.avg();
-
-		Calculator c2 = new Calculator();
-		c2.setOp(20, 40);
-		c2.sum();
-		c2.avg();
-
-		System.out.println(Calculator.PI);
-
+class SubstractCalc extends Calculator {
+	public void substract() {
+		System.out.println(this.left - this.right);
 	}
 }
+	public class ObjectCalcEx {
+		public static void main(String[] args) {
+			Calculator c1 = new Calculator();
+			c1.setOp(10, 20);
+			c1.sum();
+			c1.avg();
+
+			Calculator c2 = new Calculator();
+			c2.setOp(20, 40);
+			c2.sum();
+			c2.avg();
+
+			System.out.println(Calculator.PI);
+
+			SubstractCalc c3 = new SubstractCalc();
+			c3.setOp(10, 20);
+			c3.substract();
+
+		}
+	}
