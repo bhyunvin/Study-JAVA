f5a6755961f0b7cae53d2f00f977aad79130d0c6
백종현 <bhyunvin@naver.com>
2019-05-04 22:23:42 +0900

    Add files via upload

--- ScannerEx3.java
+++ ScannerEx3.java
@@ -10,12 +10,12 @@ public class ScannerEx3 {
 		System.out.print("비밀번호를 입력해주세요 = ");
 		String password = sc.nextLine();
 		sc.close();
-		if (id.equals("bhyunvin")) {
-			if (password.equals("1111")) {
-				System.out.println("로그인되었습니다.");
-			} else {
-				System.out.println("로그인에 실패했습니다.");
-			}
+		if (id.equals("bhyunvin") && password.equals("1111")) {
+			System.out.println("로그인에 성공했습니다.");
+		} if(id.equals("bhyunvin") && !password.equals("1111")) {
+			System.out.println("비밀번호가 틀렸습니다.");
+		} if (!id.equals("bhyunvin") && password.equals("1111")) {
+			System.out.println("등록되지 않은 사용자입니다.");
 		}
 	}
 }
